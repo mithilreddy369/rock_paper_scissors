fb0caf08ba4c2dcfefb3e307a38cade96f8ec939
mithilreddy369 <89974832+mithilreddy369@users.noreply.github.com>
2023-12-19 20:05:22 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,17 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
 
-url = "https://livecounts.io/youtube-live-subscriber-counter/UCX6OQ3DkcsbYNE6H8uQQuVA"
-response = requests.get(url)
-
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    subscriber_count_element = soup.find('span', {'class': 'odometer-inside'})
-    
-    if subscriber_count_element:
-        subscriber_count = subscriber_count_element.text.strip()
-        print(f"Subscriber Count: {subscriber_count}")
-    else:
-        print("Subscriber count element not found.")
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
